e4b52366efc47a344953ef190c38ffbfbd176f71
lamazoshvilil <lamazoshvilil7@gmail.com>
2024-08-17 12:29:13 +0400

    task7

---
+++ game.js
@@ -0,0 +1,10 @@
+function cratebutton(game){
+     const button = document.createElement(`button`);
+     // button.addEventListener(`click` (e) => {
+
+     // }
+     button.style.width = `200 px`;
+     button.style.margin = `auto`;
+     button.style.height = `100px`;
+}
+const button = document.createElement(`button`);
